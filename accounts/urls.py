019e513026ac288391c425cb8018aecebeb518fd
rkgus1251 <gahyun1251@gmail.com>
2022-01-18 23:08:02 +0900

    로그아웃 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
@@ -7,7 +7,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
-    path('signout/', views.signout, name='signout'),
+    path('signout/', auth_views.LogoutView.as_view(), name='signout'),
     path('find_username/', views.find_username, name='find_username'),
     path('signin/kakao/', views.kakao_login, name="kakao_signin"),
     path('signin/kakao/callback/', views.kakao_login_callback, name="kakao_signin_callback"),
